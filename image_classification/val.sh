3a299b1478824ca424a1ddb19c0fb020272b8f89
DoranLyong <cheeryun@gmail.com>
2024-01-21 17:37:41 +0900

    Update val.sh

--- image_classification/val.sh
+++ image_classification/val.sh
@@ -1,1 +1,5 @@
-python validate.py /path/to/imagenet --model spanet_medium -b 128 --checkpoint ./ckpt/spanet-medium.pth
+DATA_PATH=/path/to/imagenet
+MODEL_NAME=spanet_medium
+CKPT_PATH=./ckpt/spanet-medium.pth
+
+python validate.py $DATA_PATH --model $MODEL_NAME -b 128 --checkpoint $CKPT_PATH
